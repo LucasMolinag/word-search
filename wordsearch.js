4a2dc777c993c490a418765c02ebe30ac853b85e
Lucas Molina <molinag.lucas@gmail.com>
2023-09-04 13:59:31 -0700

    Working copy

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (word.length === 0) return false; // Handle the case of an empty word
+
+  const numRows = letters.length;
+  const numCols = letters[0].length;
+
+  // Check horizontally
+  for (let row = 0; row < numRows; row++) {
+    const rowString = letters[row].join('');
+    if (rowString.includes(word)) return true;
+  }
+
+  // Check vertically
+  for (let col = 0; col < numCols; col++) {
+    let colString = '';
+    for (let row = 0; row < numRows; row++) {
+      colString += letters[row][col];
     }
+    if (colString.includes(word)) return true;
+  }
+
+  return false; // Word not found in either direction
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
